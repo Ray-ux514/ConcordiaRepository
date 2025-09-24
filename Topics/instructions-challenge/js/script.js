38d7958dfba868b2e90dfcb27ab8710a5c67785e
Ray-ux514 <rayvhaunn@hotmail.com>
2025-09-24 16:28:08 -0400

    Added sun and sky

--- Topics/instructions-challenge/js/script.js
+++ Topics/instructions-challenge/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Title of Project
  * Author Name
- * 
+ *
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
@@ -10,15 +10,24 @@
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ */
 function setup() {
-
+  createCanvas(1500, 1500);
+  background(135, 206, 235);
 }
-
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ */
 function draw() {
+  fill(34, 139, 34);
+  noStroke();
+  ellipse(width / 2, height, width * 1.5, height);
 
+  fill(46, 139, 87);
+  noStroke();
+  ellipse(width / 1.5, height, width, height * 1);
+
+  fill(255, 255, o);
+  ellipsoid(300);
 }
