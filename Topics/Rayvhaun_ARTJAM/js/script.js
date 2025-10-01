4d1a640ba4bd60644ff135c7d6b77a92e22d8028
Ray-ux514 <rayvhaunn@hotmail.com>
2025-10-01 19:28:05 -0400

    adding a goatee

--- Topics/Rayvhaun_ARTJAM/js/script.js
+++ Topics/Rayvhaun_ARTJAM/js/script.js
@@ -51,12 +51,61 @@ function draw() {
   push(); //
   fill(230, 247, 252); //ear colour//
   noStroke();
-  ellipse(482, 335, 15, 15); // Face oval
+  ellipse(482, 335, 12, 12); // Face oval
   pop();
 
   push();
   fill(17, 11, 4); // brown
   noStroke();
   arc(300, 300, 350, 365, PI, TWO_PI, CHORD);
+  pop();
+
+  push();
+  noStroke();
+  fill(126, 84, 34); // brown hair
+  rect(125, 280, 350, 30);
+  pop();
+  //hairline//
+  push();
+  noStroke();
+  fill(126, 84, 34); // brown hair
+  rect(160, 240, 285, 40);
+  pop();
+
+  //goatee//
+  push(); //
+  fill(17, 11, 4); // Skin tone
+  noStroke();
+  ellipse(330, 479, 14, 14); // Face oval
+  pop();
+
+  push();
+  fill(17, 11, 4); // Skin tone
+  noStroke();
+  ellipse(300, 479, 14, 14); // Face oval
+  pop();
+
+  push(); //
+  fill(17, 11, 4); // Skin tone
+  noStroke();
+  ellipse(295, 479, 14, 14); // Face oval
+  pop();
+
+  push(); //
+  fill(17, 11, 4); // Skin tone
+  noStroke();
+  ellipse(307, 486, 14, 14); // Face oval
+  pop();
+
+  push(); //
+  fill(17, 11, 4); // Skin tone
+  noStroke();
+  ellipse(308, 479, 14, 14); // Face oval
+  pop();
+
+  push(); //
+  fill(17, 11, 4); // Skin tone
+  noStroke();
+  ellipse(295, 490, 14, 14); // Face oval
   pop();
 }
