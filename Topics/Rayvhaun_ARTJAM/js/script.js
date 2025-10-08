86286a566ef61578c084c42be71d1710bd214a39
Ray-ux514 <rayvhaunn@hotmail.com>
2025-10-08 11:44:51 -0400

    Adjusting facial features

--- Topics/Rayvhaun_ARTJAM/js/script.js
+++ Topics/Rayvhaun_ARTJAM/js/script.js
@@ -1,149 +1,162 @@
-/**
- * Title of Project
- * Author Name
- *
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
- */
+// === Eye setup ===
+const leftEye = {
+  x: 230,
+  y: 340,
+  w: 70,
+  h: 60,
+  moveRange: 10, // how much the pupil slides
+};
+
+const rightEye = {
+  x: 375,
+  y: 340,
+  w: 70,
+  h: 60,
+  moveRange: 10,
+};
+
+let leftBrowAngle = 10;
+let rightBrowAngle = 170;
+let rotateBrow = false;
+
+// === Base drawing ===
 function setup() {
   createCanvas(600, 600);
 }
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
- */
 function draw() {
   background("#aaaaaa");
 
-  //ears//
-  push(); //
-  fill(126, 79, 33); // Skin tone
-  stroke(35, 31, 3); // Outline color (dark brown)
-  strokeWeight(15);
-  ellipse(100, 330, 85, 85); // Face oval
-  pop();
+  drawEars();
+  drawFace();
+  drawHair();
+  drawEarrings();
+  drawEyebrows();
+  drawEyes(); // whites + arcs + pupils (with movement)
+  drawBeard();
+  drawMouth();
+}
 
-  //ears//
-  push(); //
-  fill(126, 79, 33); // Skin tone
-  stroke(35, 31, 3); // Outline color (dark brown)
-  strokeWeight(15);
-  ellipse(500, 330, 85, 85); // Face oval
-  pop();
-
-  // face//
+// === Helper draw functions ===
+function drawEars() {
   push();
-  fill(126, 79, 33); // Skin tone
-  stroke(35, 31, 3); // Outline color (dark brown)
-  strokeWeight(15); // Outline thickness
-  ellipse(300, 300, 380, 380); // Face oval
+  fill(126, 79, 33);
+  stroke(35, 31, 3);
+  strokeWeight(15);
+  ellipse(100, 330, 85, 85);
+  ellipse(500, 330, 85, 85);
   pop();
+}
 
-  //hair//
+function drawFace() {
   push();
-  stroke(35, 31, 3); // Outline color (dark brown)
+  fill(126, 79, 33);
+  stroke(35, 31, 3);
+  strokeWeight(15);
+  ellipse(300, 300, 380, 380);
+  pop();
+}
+
+function drawHair() {
+  push();
+  stroke(35, 31, 3);
   strokeWeight(10);
-  fill(35, 31, 32); // Skin tone
+  fill(35, 31, 32);
   arc(300, 300, 380, 380, PI, TWO_PI, CHORD);
   pop();
 
-  //earigns//
-  push(); //
-  fill(206, 235, 241); //ear colour//
-  noStroke();
-  ellipse(100, 372, 20, 20); // Face oval
-  pop();
-
-  push(); //
-  fill(255, 255, 255); //ear colour//
-  noStroke();
-  ellipse(100, 372, 14, 14); // Face oval
-  pop();
-
-  //earigns//
-  push(); //
-  fill(206, 235, 241); //ear colour//
-  noStroke();
-  ellipse(500, 372, 20, 20); // Face oval
-  pop();
-
-  push(); //
-  fill(255, 255, 255); //ear colour//
-  noStroke();
-  ellipse(500, 372, 14, 14); // Face oval
-  pop();
-
   push();
   noStroke();
-  fill(126, 79, 33); // brown hair
+  fill(126, 79, 33);
   rect(150, 240, 300, 90);
   pop();
+}
 
-  // eyebrows//
+function drawEarrings() {
   push();
   noStroke();
-  fill(35, 31, 3); // brown hair
-  translate(225, 310); // move to where you want the rectangle
-  rotate(radians(7)); // slight rotation (5 degrees)
-  rectMode(CENTER); // draw from center for even rotation
-  rect(0, 0, 105, 15); // rectangle at the new origin
+  fill(206, 235, 241);
+  ellipse(100, 372, 20, 20);
+  ellipse(500, 372, 20, 20);
+  fill(255);
+  ellipse(100, 372, 14, 14);
+  ellipse(500, 372, 14, 14);
   pop();
-  push();
+}
 
-  noStroke();
-  fill(35, 31, 3, 70); // brown hair
-  translate(265, 330); // move to where you want the rectangle
-  rotate(radians(7)); // slight rotation (5 degrees)
-  rectMode(CENTER); // draw from center for even rotation
-  rect(0, 0, 17, 5); // rectangle at the new origin
-  pop();
-
+function drawEyebrows() {
   push();
   noStroke();
-  fill(35, 31, 3); // brown hair
-  translate(380, 310); // move to where you want the rectangle
-  rotate(radians(173)); // slight rotation (5 degrees)
-  rectMode(CENTER); // draw from center for even rotation
-  rect(0, 0, 105, 15); // rectangle at the new origin
+  fill(35, 31, 3);
+  drawRotRect(230, 310, 100, 15, 10);
+  drawRotRect(375, 310, 100, 15, 170);
+  fill(35, 31, 3, 70);
+  drawRotRect(265, 330, 22, 5, 10);
+  drawRotRect(340, 330, 22, 5, 170);
   pop();
+}
 
+function drawRotRect(x, y, w, h, angleDeg) {
+  push();
+  translate(x, y);
+  rotate(radians(angleDeg));
+  rectMode(CENTER);
+  rect(0, 0, w, h);
+  pop();
+}
+
+// === Eyes (with movement) ===
+function drawEyes() {
+  // Whites
   push();
   noStroke();
-  fill(35, 31, 3, 70); // brown hair
-  translate(340, 330); // move to where you want the rectangle
-  rotate(radians(173)); // slight rotation (5 degrees)
-  rectMode(CENTER); // draw from center for even rotation
-  rect(0, 0, 17, 5); // rectangle at the new origin
+  fill(255);
+  arc(230, 340, 95, 85, 0, PI, CHORD);
+  arc(375, 340, 95, 85, 0, PI, CHORD);
   pop();
 
-  //eyes//
+  // Eye arcs / lashes
+  push();
+  stroke(35, 31, 3, 70);
+  strokeWeight(5);
+  noFill();
+  strokeCap(SQUARE);
+  arc(365, 348, 95, 85, HALF_PI, PI);
+  arc(240, 348, 95, 85, 0, HALF_PI);
+  pop();
+
+  // Moving pupils
+  drawMovingEye(leftEye);
+  drawMovingEye(rightEye);
+}
+
+function drawMovingEye(eye) {
+  let moveX = map(mouseX, 0, width, -eye.moveRange, eye.moveRange);
   push();
   noStroke();
-  fill(0, 0, 0); // Skin tone
-  arc(230, 270, 180, 180, PI, TWO_PI, CHORD);
+  fill(35, 31, 3);
+  arc(eye.x + moveX, eye.y, eye.w, eye.h, 0, PI, CHORD);
   pop();
+}
 
-  //beard//
-  push(); //
-  stroke(35, 31, 3); // Outline color (dark brown)
-  strokeWeight(0); // Outline thickness
-  fill(35, 31, 3); // brown hair
+// === Beard and mouth ===
+function drawBeard() {
+  push();
+  noStroke();
+  fill(35, 31, 3);
   ellipse(300, 480, 90, 30);
   pop();
+}
 
-  //mouth//
+function drawMouth() {
   push();
   noStroke();
-  fill(35, 31, 3); // brown hair
-  translate(300, 430); // move to where you want the rectangle
-  rotate(radians(175)); // slight rotation (5 degrees)
-  rectMode(CENTER); // draw from center for even rotation
-  rect(0, 0, 85, 14); // rectangle at the new origin
+  fill(35, 31, 3);
+  translate(300, 430);
+  rotate(radians(175));
+  rectMode(CENTER);
+  rect(0, 0, 85, 14);
   pop();
 }
