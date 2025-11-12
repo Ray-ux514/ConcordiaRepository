e01964b2bb57877b0d82118a7ee5838ea6c48b28
Ray-ux514 <rayvhaunn@hotmail.com>
2025-11-12 18:33:36 -0500

    class example on data

---
+++ Topics/data-example/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Title of Project
+ * Author Name
+ *
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ */
+
+const cell_size = 125;
+const NUM_COLS = 4;
+const NUM_ROWS = 4;
+
+let currentMaze = null;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ */
+function draw() {
+  background(0);
+}
