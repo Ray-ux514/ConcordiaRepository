1bc2047e8e21233747b6098da300135d44c394f1
Ray-ux514 <rayvhaunn@hotmail.com>
2025-12-03 03:13:49 -0500

    Starting lotus game

---
+++ Topics/VariationJam/js/lotusbloom.js
@@ -0,0 +1,15 @@
+//=========
+// Lotus Bloom
+//===
+
+"use sctrict";
+
+let lotusimg;
+
+//game state
+let buds = [];
+let score = 0;
+let lives = 3;
+let gameOver = false;
+
+const GAME_TIME = 30;
