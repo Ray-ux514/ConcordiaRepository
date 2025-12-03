78acc9e9fc4e32f6c82c02c099b792e8f76b68eb
Ray-ux514 <rayvhaunn@hotmail.com>
2025-12-03 11:23:19 -0500

    Updates
    Adding fonts and setup to lotus

--- Topics/VariationJam/js/lotusbloom.js
+++ Topics/VariationJam/js/lotusbloom.js
@@ -5,6 +5,8 @@
 "use sctrict";
 
 let lotusimg;
+let font1;
+let font2;
 
 //game state
 let buds = [];
@@ -13,3 +15,26 @@ let lives = 3;
 let gameOver = false;
 
 const GAME_TIME = 30;
+let gameStartTime = 0;
+
+// bud spawning
+let lastSpawnTime = 0;
+let spawnInterval = 900; // ms – will randomize a bit
+
+// basic bud settings
+const BUD_LIFESPAN = 2500; // ms before it wilts
+const BUD_RADIUS = 26;
+
+// font
+function preload() {
+  lotusImg = loadImage("assets/lotus.png"); // <-- put your image here
+  font1 = loadFont("assets/fonts/ABCMaxiPlusVariable-Trial.ttf");
+  font2 = loadFont("assets/fonts/ABCGinto-Regular-Trial.otf");
+}
+
+function setup() {
+  createCanvas(850, 500);
+  ImageMode(CENTER);
+  textFont("Inter");
+  resetGame();
+}
